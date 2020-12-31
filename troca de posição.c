19c967a335561c2e6c01e5d5cc1c24fce2c837dd
DirceuSilvestre <60676585+DirceuSilvestre@users.noreply.github.com>
2020-12-30 22:13:11 -0300

    Update troca de posição.c

--- troca de posição.c
+++ troca de posição.c
@@ -26,3 +26,30 @@ int main(int argc, char const *argv[])
     }
     return 0;
 }
+
+void troca(int v[])
+{
+    int c = S-1;
+    for (int i = 0; i < (S/2); ++i)
+    {
+        v[i] += v[c];
+        v[c] = v[i] - v[c];
+        v[i] -= v[c];
+        c--;
+    }
+}
+            
+int main(int argc, char const *argv[])
+{
+    int a[S], c;
+    for (int i = 0; i < S; ++i)
+    {
+        scanf("%d", &a[i]); 
+    }
+    troca(a);
+    for (int i = 0; i < S; ++i)
+    {
+        printf("%d\n", a[i]);
+    }
+    return 0;
+}
