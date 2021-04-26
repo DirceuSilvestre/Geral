e70145155b0c5e434d518ee109dad5d68089edc7
DirceuSilvestre <60676585+DirceuSilvestre@users.noreply.github.com>
2021-04-26 20:48:02 -0300

    Update slides.c

--- slides.c
+++ slides.c
@@ -46,6 +46,10 @@ Estruturas Básicas:
 		}
 
 A função principal de C, unica que precisa estar presente é a: MAIN()
+	#Main
+		Recebe os parâmetros (int argc; int *argv []), mas também pode ser void
+			argc são os números que pode receber e são passados após o ./teste no terminal separados por ' ' espaço
+			*argv [] é um vetor de strings, o argv[0] costuma ser o ./teste digitado no terminal
 		
 Para executar algumas funções, como as mais básicas, deve chamar a biblioteca respectiva dessa forma: #INCLUDE <BIBLIOTECA.H>
 
